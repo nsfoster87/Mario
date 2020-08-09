52ae7e78b9d059cc0ec2372fdbde597a7e398094
Nate F <nsfoster87@yahoo.com>
2020-08-09 16:01:46 -0400

    Update mario1.c

--- mario1.c
+++ mario1.c
@@ -1,44 +1,51 @@
 #include <stdio.h>
 #include <cs50.h>
 
+void getHeightFromUser(void);
+void printPyramids(void);
+void printFirstPyramidOnRow(int row);
+void printSecondPyramidOnRow(int row);
+
+int height;
+
 int main(void)
 {
-    // declare height var for storing user input
-    int height;
-    
-    // get a user input (1-8) and store it in height var
-    do
-    {
-        height = get_int("Height: ");
-    }
-    while (height < 1 || height > 8);
-    
-    // iterate through each row
-    for (int i = 1; i < height + 1; i++)
-    {
-        // print first pyramid
-        for (int j = height; j > 0; j--)
-        {
-            if (j > i)
-            {
-                printf(" ");
-            }
-            else
-            {
-                printf("#");
-            }
-        }
-        
-        // print gap
-        printf("  ");
-        
-        // print second pyramid
-        for (int j = 0; j < i; j++)
-        {
-            printf("#");
-        }
-        
-        // new line
-        printf("\n");
-    }
+  getHeightFromUser();
+  printPyramids();
+}
+
+void getHeightFromUser(void)
+{
+  do
+  {
+      height = get_int("Height: ");
+  }
+  while (height < 1 || height > 8);
+}
+
+void printPyramids(void)
+{
+  for (int i = 1; i < height + 1; i++)
+  {
+    printFirstPyramidOnRow(i);
+    printf("  ");
+    printSecondPyramidOnRow(i);
+    printf("\n");
+  }
+}
+
+void printFirstPyramidOnRow(int row)
+{
+  for (int j = height; j > 0; j--)
+  {
+      (j > row) ? printf(" ") : printf("#");
+  }
+}
+
+void printSecondPyramidOnRow(int row)
+{
+  for (int j = 0; j < row; j++)
+  {
+      printf("#");
+  }
 }
